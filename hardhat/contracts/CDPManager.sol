ec0c0f3749ec81f189f2928981bc17751cb27d32
Hajduk Dušan <hajdukdusan99@gmail.com>
2022-07-25 17:05:49 +0200

    Mint And Burn, Fix Noi

--- hardhat/contracts/CDPManager.sol
+++ hardhat/contracts/CDPManager.sol
@@ -1,5 +1,9 @@
 pragma solidity >=0.8.0 <0.9.0;
 
+import "./NOI.sol";
+
+error CDPManager__NotAuthorized();
+
 contract CDPManager {
 
     struct CDP {
@@ -9,9 +13,17 @@ contract CDPManager {
         uint256 generatedDebt; // [wad]
     }
 
-    mapping(address => CDP[]) cdpListPerUser; // Owner => CDP[]
+    mapping(address => CDP[]) private cdpListPerUser; // Owner => CDP[]
 
-    constructor() {
+    NOI private immutable NOI_COIN;
+
+    modifier HasAccess(address _user) {
+        if(msg.sender != _user) revert CDPManager__NotAuthorized();
+        _;
+    }
+
+    constructor(address _noiCoin) {
+        NOI_COIN = NOI(_noiCoin);
     }
 
     // Open a new cdp for a given _user address.
@@ -22,5 +34,13 @@ contract CDPManager {
     //Adds collateral to an existing CDP
     function transferCollateralToCDP(address _user,uint _cdpIndex) public payable{
         cdpListPerUser[_user][_cdpIndex].lockedCollateral=cdpListPerUser[_user][_cdpIndex].lockedCollateral+msg.value;
+    }
+
+    function mintFromCDP(address _user, uint256 _cdpIndex, uint256 _amount) public HasAccess(_user) {
+        NOI_COIN.mint(_user, _amount);
+    }
+
+    function repayToCDP(address _user, uint256 _cdpIndex, uint256 _amount) public HasAccess(_user){
+        NOI_COIN.burn(_user, _amount);
     }
 }
