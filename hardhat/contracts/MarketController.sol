dacbee81111f278fbd35ee401b2c0b78d5990df0
Mihajlo426 <abed.mv2k@gmail.com>
2022-07-27 10:48:04 +0200

    Add Controller contracts

---
+++ hardhat/contracts/MarketController.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+contract CPIController{
+
+
+    constructor(){
+
+    }
+
+    function computeRate(uint256 _rp, uint _mp) public pure returns (uint256){
+        return 0;
+    } 
+    
+}
--- hardhat/contracts/RateSetter.sol
+++ hardhat/contracts/RateSetter.sol
@@ -4,22 +4,37 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "hardhat/console.sol";
 import "./CDPManager.sol";
+import "./MarketController.sol";
+import "./CPIController.sol";
 
 contract RateSetter {
 
+    uint256 redemptionPrice;
+    uint256 marketPrice;
+    uint256 CPI;
+    uint256 redemptionRate;
+    uint256 ethPrice;
+
     CDPManager private immutable CDPManager_CONTARCT;
+    CPIController private immutable CPIController_CONTRACT;
+    MarketController private immutable MarketController_CONTRACT;
 
     constructor(address _cdpManager) {
         CDPManager_CONTARCT = CDPManager(_cdpManager);
+
     }
 
     /*
      * @notice updates rates with values gathered from PI controllers
      */
-    function updateRates() public{
+    function updateCDPManagerData() public{
         // gather rate from market/redemption controller
         // gather rate from CPI controller
         CDPManager_CONTARCT.updateValue(900);
+    }
+
+    function updateRatesInternal() public {
+
     }
 
 }
