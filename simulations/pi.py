7f571be60a6c94799c7dfcfc6e93bed66dc43df6
Marko Macura <marko.macura9@gmail.com>
2022-07-27 14:55:20 +0200

    Prepair PID for testing

--- simulations/pi.py
+++ simulations/pi.py
@@ -1,37 +1,138 @@
-import numpy as np
+import math
 import matplotlib.pyplot as plt
+import numpy as np
 
 TIME_STEP = 0.01
+NEGATIVE_RATE_LIMIT = 50
 
-def proportional_rate(Kp: float, err: float) -> float:
-    return Kp * err
+class ControllerGains(object):
+    def __init__(self, Kp: float, Ki: float):
+        self.Kp = Kp
+        self.Ki = Ki
 
-def integral_rate(integral: float, Ki: float, err_t: float, err_t_1: float, alpha: float) -> float:
-    return integral * alpha + Ki * (err_t + err_t_1)/2 * TIME_STEP
+class DeviationObservation(object):
+    def __init__(self, timestamp: float, proportional: float, integral: float):
+        self.timestamp = timestamp
+        self.proportional = proportional
+        self.integral = integral
 
-def market(value: float) -> float:
-    return value
+def getLastProportionalTerm() -> float:
+    if oll() == 0:
+        return 0
+    return deviationObservations[oll() - 1].proportional
 
-if __name__ == "__main__":
-    integral = 0
-    time = np.arange(500)
-    reference = np.zeros(500) + 1
-    output = np.zeros(500)
-    Kp = 0.9
-    Ki = 3
+def getLastIntegralTerm() -> float:
+    if oll() == 0:
+        return 0
+    return deviationObservations[oll() - 1].integral
 
-    for i in range(1, 500):
-        err_t = reference[i] - output[i-1]
-        if i != 1:
-            err_t_1 = reference[i-1] - output[i-2]
+def oll() -> int:
+    return len(deviationObservations)
+
+def getBoundedRedemptionRate(piOutput: float) -> tuple:
+    global defaultRedemptionRate
+    global defaultGlobalTimeline
+    global feedbackOutputUpperBound
+    global feedbackOutputLowerBound
+    boundedPIOutput = piOutput
+    boundedPIOutput = feedbackOutputLowerBound
+
+    if piOutput < feedbackOutputLowerBound:
+        boundedPIOutput = feedbackOutputLowerBound
+    elif piOutput > feedbackOutputUpperBound:
+        boundedPIOutput = int(feedbackOutputUpperBound)
+
+    negativeOutputExceedsHundred = boundedPIOutput < 0 and -boundedPIOutput >= defaultRedemptionRate
+    if (negativeOutputExceedsHundred): 
+        newRedemptionRate = NEGATIVE_RATE_LIMIT
+    else:
+        if boundedPIOutput < 0 and boundedPIOutput <= -int(NEGATIVE_RATE_LIMIT): 
+            newRedemptionRate = defaultRedemptionRate - NEGATIVE_RATE_LIMIT
         else:
-            err_t_1 = 0
-        integral = integral_rate(integral, Ki, err_t, err_t_1, 0.9)
-        output[i] = market(proportional_rate(Kp, err_t) + integral)
+            newRedemptionRate = defaultRedemptionRate + boundedPIOutput
 
-    plt.figure()
-    plt.plot(time * TIME_STEP, output)
-    plt.plot(time * TIME_STEP, reference)
-    plt.xlabel("time")
-    plt.legend(["response", "reference"])
-    plt.savefig("images/pi.png")
+    return (newRedemptionRate, defaultGlobalTimeline)
+
+def breaksNoiseBarrier(piSum: float, redemptionPrice: float) -> bool:
+    global noiseBarrier
+    deltaNoise = 2 - noiseBarrier
+    return piSum >= redemptionPrice * deltaNoise - redemptionPrice
+
+def getNextPriceDeviationCumulative(proportionalTerm: float, accumulatedLeak: float) -> tuple:
+    global priceDeviationCumulative
+
+    lastProportionalTerm = getLastProportionalTerm()
+    if lastUpdateTime == 0:
+        timeElapsed = 0
+    else:
+        timeElapsed = TIME_STEP
+    
+    newTimeAdjustedDeviation = riemannSum(proportionalTerm, lastProportionalTerm) * timeElapsed
+    leakedPriceCumulative = accumulatedLeak * priceDeviationCumulative
+
+    return (leakedPriceCumulative + newTimeAdjustedDeviation, newTimeAdjustedDeviation)
+
+def riemannSum(x: float, y: float) -> float:
+    return (x + y) / 2
+
+def absolute(x: float) -> float:
+    return math.abs(x)
+
+def getGainAdjustedPIOutput(proportionalTerm: float, integralTerm: float) -> float:
+    (adjustedProportional, adjustedIntegral) = getGainAdjustedTerms(proportionalTerm, integralTerm)
+    return adjustedProportional+ adjustedIntegral
+
+def getGainAdjustedTerms(proportionalTerm: float, integralTerm: float) -> tuple:
+    global controllerGains
+    return proportionalTerm * controllerGains.Kp, integralTerm * controllerGains.Ki
+
+# --- Rate Validation/Calculation ---
+def computeRate(marketPrice: float, redemptionPrice: float, accumulatedLeak: float) -> float:
+    global lastUpdateTime
+    global priceDeviationCumulative
+    scaledMarketPrice = marketPrice
+    proportionalTerm = redemptionPrice - scaledMarketPrice
+    updateDeviationHistory(proportionalTerm, accumulatedLeak)
+    lastUpdateTime = lastUpdateTime + TIME_STEP
+    piOutput = getGainAdjustedPIOutput(proportionalTerm, priceDeviationCumulative)
+    if breaksNoiseBarrier(absolute(piOutput), redemptionPrice) and piOutput != 0:
+        newRedemptionRate, _ = getBoundedRedemptionRate(piOutput)
+        return newRedemptionRate
+    else:
+        return 1
+
+def updateDeviationHistory(proportionalTerm: float, accumulatedLeak: float) -> None:
+    global historicalCumulativeDeviations
+    global deviationObservations
+    global lastUpdateTime
+
+    (virtualDeviationCumulative, _) = getNextPriceDeviationCumulative(proportionalTerm, accumulatedLeak)
+    priceDeviationCumulative = virtualDeviationCumulative
+    historicalCumulativeDeviations.append(priceDeviationCumulative)
+    deviationObservations.append(DeviationObservation(lastUpdateTime + TIME_STEP, proportionalTerm, priceDeviationCumulative))
+
+def getNextRedemptionRate(marketPrice: float, redemptionPrice: float, accumulatedLeak: float) -> tuple:
+    global defaultGlobalTimeline
+    scaledMarketPrice = marketPrice
+    
+    proportionalTerm = (redemptionPrice - scaledMarketPrice) / redemptionPrice
+    (cumulativeDeviation, _) = getNextPriceDeviationCumulative(proportionalTerm, accumulatedLeak)
+    piOutput = getGainAdjustedPIOutput(proportionalTerm, cumulativeDeviation)
+    if breaksNoiseBarrier(absolute(piOutput), redemptionPrice) and piOutput != 0:
+        (newRedemptionRate, rateTimeline) = getBoundedRedemptionRate(piOutput)
+        return (newRedemptionRate, proportionalTerm, cumulativeDeviation, rateTimeline)
+    else:
+        return (1, proportionalTerm, cumulativeDeviation, defaultGlobalTimeline)
+
+deviationObservations = []
+defaultRedemptionRate = 1
+defaultGlobalTimeline = 1
+historicalCumulativeDeviations = []
+feedbackOutputUpperBound = 50
+feedbackOutputLowerBound = -50
+integralPeriodSize = TIME_STEP
+controllerGains = ControllerGains(0.9, 1.5)
+priceDeviationCumulative = 0
+noiseBarrier = 0.05
+lastUpdateTime = 0
+historicalCumulativeDeviations.append(priceDeviationCumulative)
