12f7c9f02333918bdca860ec146823eec8d9da44
Marko Macura <marko.macura9@gmail.com>
2022-08-09 15:00:47 +0200

    Add primitive fuzzy

--- simulations/controllers/fuzzy_module.py
+++ simulations/controllers/fuzzy_module.py
@@ -7,18 +7,14 @@ def calculate_rp_and_rr(market_twap, redemption_price, accumulated_leak, ext_dat
     rr_cpi = computeRateCPI(market_twap, redemption_price, accumulated_leak)
     err_stable_perc = abs(market_twap - redemption_price) / redemption_price
     err_cpi_perc = abs(market_twap - ext_data.get_cpi_value()) / redemption_price
-    w_stable = 0
-    w_cpi = 0
+
+    w_stable = 0.5
     if err_stable_perc > 0.1:
-        w_stable, w_cpi = 1, 0
-    elif err_stable_perc > 0.05:
-        w_stable, w_cpi = 0.95, 0.05
-    elif err_stable_perc > 0.04:
-        w_stable, w_cpi = 0.9, 0.1
-    elif err_stable_perc > 0.03:
-        w_stable, w_cpi = 0.85, 0.15
-    else:
-        w_stable, w_cpi = 0.8, 0.2
+        w_stable = 1
+    elif err_stable_perc > 0:
+        w_stable = 0.5 + 0.5 * err_stable_perc/0.1
+        w_stable = w_stable + (1 - w_stable) * err_stable_perc/(err_stable_perc + err_cpi_perc)
+    w_cpi = 1 - w_stable
     rr = w_stable * rr_stable + w_cpi * rr_cpi
 
 
