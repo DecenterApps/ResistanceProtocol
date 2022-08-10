1a54e2b05b827c068317c41a03424fcd37442df9
Marko Macura <marko.macura9@gmail.com>
2022-08-10 14:43:55 +0200

    Add fix prediction

--- simulations/utils/regression.py
+++ simulations/utils/regression.py
@@ -2,20 +2,16 @@ import numpy as np
 from sklearn import svm
 from sklearn.preprocessing import StandardScaler
 
-def svm_prediction(data: np.array, time_stamp: float):
-
+def svm_training(data: np.array):
     X_l = np.arange(data.size)
-    StdS_X = StandardScaler()
-    StdS_y = StandardScaler()
     X_l = StdS_X.fit_transform(X_l)
     data = StdS_y.fit_transform(data)
 
     regression = svm.SVR(kernel = 'rbf')
     regression.fit(X_l, data)
 
+def svm_prediction(regression: svm.SVR, time_stamp: float):
     return regression.predict(StdS_X.transform([[time_stamp]]))
 
-
-if __name__ == "__main__":
-    clf = svm.SVC()
-    print("x")
+StdS_X = StandardScaler()
+StdS_y = StandardScaler()
