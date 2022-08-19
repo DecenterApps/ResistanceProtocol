0ecbe873d0a2f0f0a7307f30c2f52700b16bee1c
Ivan Jevtic <ivanjevtic501@gmail.com>
2022-08-19 16:48:20 +0200

    update readme

---
+++ simulations/parameters.py
@@ -0,0 +1,37 @@
+
+parameters = {
+    'leverager': 5,
+    'safe_owner': 5
+},
+{
+    'leverager': 0,
+    'safe_owner': 0
+},
+{
+    'leverager': 10,
+    'safe_owner': 10
+},
+{
+    'leverager': 15,
+    'safe_owner': 15
+},
+{
+    'leverager': 20,
+    'safe_owner': 20
+},
+{
+    'leverager': 25,
+    'safe_owner': 25
+},
+{
+    'leverager': 30,
+    'safe_owner': 30
+},
+{
+    'leverager': 40,
+    'safe_owner': 40
+},
+{
+    'leverager': 50,
+    'safe_owner': 50
+}
--- simulations/sim1.py
+++ simulations/sim1.py
@@ -14,6 +14,7 @@ from utils.exchange import *
 from agents.agent_utils import *
 from utils.constants import *
 from tqdm import tqdm
+from parameters import parameters
 import csv
 
 
@@ -127,42 +128,7 @@ sim_config_dict = {
     'N': 1,
     'M': {
         'parameters': [
-            {
-                'leverager': 5,
-                'safe_owner': 5
-            },
-            {
-                'leverager': 0,
-                'safe_owner': 0
-            },
-            {
-                'leverager': 10,
-                'safe_owner': 10
-            },
-            {
-                'leverager': 15,
-                'safe_owner': 15
-            },
-            {
-                'leverager': 20,
-                'safe_owner': 20
-            },
-            {
-                'leverager': 25,
-                'safe_owner': 25
-            },
-            {
-                'leverager': 30,
-                'safe_owner': 30
-            },
-            {
-                'leverager': 40,
-                'safe_owner': 40
-            },
-            {
-                'leverager': 50,
-                'safe_owner': 50
-            },
+            parameters
         ]
     }
 }
